a0121033464db3b7039e058f64bbf043bbdc398e
ADdream <varma.kalidhindi931@gmail.com>
2020-10-06 20:55:13 -0500

    added cages data model

---
+++ src/data/cages.py
@@ -0,0 +1,23 @@
+import mongoengine
+import datetime
+from data.bookings import Booking
+
+class Cage(mongoengine.Document):
+
+    registered_date = mongoengine.DateTimeField(default=datetime.datetime.now)
+    name = mongoengine.DateTimeField(required=True)
+    cost = mongoengine.FloatField(required=True)
+    square_meters = mongoengine.FloatField(required=True)
+    is_carpented = mongoengine.BooleanField(requried=True)
+    has_toys = mongoengine.BooleanField(required=True)
+    allow_dangerous_snakes = mongoengine.BooleanField(required=True)
+
+    bookings = mongoengine.EmbeddedDocumentListField(Booking)
+
+    meta = {
+        'db_alias': 'core',
+        'collection': 'cages'
+
+    }
+
+
